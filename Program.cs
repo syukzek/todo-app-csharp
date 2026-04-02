44287363342f727b6e71bb0c6cee4160fb6b94e1
syukzek <syukzek@gmail.com>
2026-04-02 22:11:47 +0800

    Cleanup project and add SQL CRUD

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
 using System;
-using System.Net;
+using Microsoft.Data.SqlClient;
 
-List<string> Listtask = new List<string>();
+string connectionString = "Server=DESKTOP-D2LNFJV\\SQLEXPRESS;Database=TodoDB;Trusted_Connection=True;";
+
 
 while (true)
 {
@@ -12,7 +11,8 @@ while (true)
     Console.WriteLine("1. Tambah tugas");
     Console.WriteLine("2. Lihat tugas");
     Console.WriteLine("3. Hapus tugas");
-    Console.WriteLine("4. Exit");
+    Console.WriteLine("4. Update tugas");
+    Console.WriteLine("5. Exit");
     string input = Console.ReadLine();
 
     if (input == "1")
@@ -20,63 +20,124 @@ while (true)
 
         Console.WriteLine("Masukkan tugas yang ingin ditambahkan: ");
         string newTask = Console.ReadLine();
-        Listtask.Add(newTask);
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.Open();
+
+        string query = "INSERT INTO Tasks (TaskName) VALUES (@task)";
+        SqlCommand cmd = new SqlCommand(query, conn);
+        cmd.Parameters.AddWithValue("@task", newTask);
+
+        cmd.ExecuteNonQuery();
+        
     }
 
     else if (input == "2")
     {
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.Open();
 
-        if (Listtask.Count == 0)
+        string query = "SELECT * FROM Tasks";
+        SqlCommand cmd = new SqlCommand(query, conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+
+        Console.WriteLine("\nTugas Anda: ");
+
+        while (reader.Read())
         {
-            Console.WriteLine("Tidak ada tugas yang tersedia.");
-
+            Console.WriteLine(reader["Id"] + ". " + reader["TaskName"]);
         }
-
-        else
-        {
-            Console.WriteLine("\nTugas Anda: ");
-            for (int i = 0; i < Listtask.Count; i++)
-            {
-                Console.WriteLine((i + 1) + "." + (Listtask[i]));
-            }
-        }
+        reader.Close();
+        
     }
 
     else if (input == "3")
     {
-   
-        if (Listtask.Count == 0)
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.Open();
+
+        // display task dulu
+        string selectQuery = "SELECT * FROM Tasks";
+        SqlCommand selectCmd = new SqlCommand(selectQuery, conn);
+        SqlDataReader reader = selectCmd.ExecuteReader();
+
+        Console.WriteLine("\nTugas Anda: ");
+
+        if (!reader.HasRows)
         {
             Console.WriteLine("Tidak ada tugas yang tersedia.");
+            reader.Close();
+            
 
         }
-
         else
         {
-            Console.WriteLine("\nTugas Anda: ");
-            for (int i = 0; i < Listtask.Count; i++)
+            while (reader.Read())
             {
-                Console.WriteLine((i + 1) + "." + (Listtask[i]));
-            }
-            Console.WriteLine("Masukkan nomor tugas yang ingin dihapus: "); 
-            string delete = Console.ReadLine();
-            int intdelete = int.Parse(delete);
-            int index = intdelete - 1;
-            if (index >= 0 && index < Listtask.Count)
-            {
-                Listtask.RemoveAt(index);
-            }
-            else
-            {
-                Console.WriteLine("Nombor tidak sah");
+                Console.WriteLine(reader["Id"] + ". " + reader["TaskName"]);
             }
 
+            reader.Close(); // penting ✅
 
+            Console.WriteLine("Masukkan ID tugas yang ingin dihapus: ");
+            int id = int.Parse(Console.ReadLine());
+
+            string deleteQuery = "DELETE FROM Tasks WHERE Id = @id";
+            SqlCommand deleteCmd = new SqlCommand(deleteQuery, conn);
+            deleteCmd.Parameters.AddWithValue("@id", id);
+
+            deleteCmd.ExecuteNonQuery();
+
+            Console.WriteLine("Tugas berjaya dihapus!");
+
+           
         }
-
     }
 
     else if (input == "4")
+    {
+        SqlConnection conn = new SqlConnection(connectionString);
+        conn.Open();
+
+        // display task dulu
+        string selectQuery = "SELECT * FROM Tasks";
+        SqlCommand selectCmd = new SqlCommand(selectQuery, conn);
+        SqlDataReader reader = selectCmd.ExecuteReader();
+
+        Console.WriteLine("\nTugas Anda: ");
+
+        if (!reader.HasRows)
+        {
+            Console.WriteLine("Tidak ada tugas.");
+            
+        }
+        else
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["Id"] + ". " + reader["TaskName"]);
+            }
+
+            reader.Close();
+
+            Console.WriteLine("Masukkan ID tugas yang ingin diupdate: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Masukkan task baru: ");
+            string newTask = Console.ReadLine();
+
+            string updateQuery = "UPDATE Tasks SET TaskName = @task WHERE Id = @id";
+            SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
+            updateCmd.Parameters.AddWithValue("@task", newTask);
+            updateCmd.Parameters.AddWithValue("@id", id);
+
+            updateCmd.ExecuteNonQuery();
+
+            Console.WriteLine("Tugas berjaya diupdate!");
+
+        }
+    }
+
+    else if (input == "5")
     {
         break;
     }
